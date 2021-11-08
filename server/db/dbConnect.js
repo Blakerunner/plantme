af89531a1bbaa312a003d5098790d3c78c0c06a3
Blake Michalzik <github+bmichalzik@gmail.com>
2021-11-08 15:17:19 -0800

    updated to allow mySQL connection via Sequelize

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,14 +1,24 @@
-const mysql = require('mysql');
-const DB = mysql.createConnection({
+// Connect to database via Sequelize ORM
+const DB = new Sequelize(
+  process.env.MYSQL_DATABASE || "plantme",
+  process.env.MYSQL_USER || "root",
+  process.env.MYSQL_PASSWORD || "",
+  {
+    dialect: "mysql",
     host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "",
-    database: process.env.MYSQL_DATABASE || "plantme",
-});
+  }
+);
 
-DB.connect( (err) => {
-    if (err) throw err;
-    console.log(`mySQL database connected - ${process.env.MYSQL_DATABASE}`);
-})
+try {
+  await DB.authenticate();
+  console.log(`Database connected successful: ${process.env.MYSQL_DATABASE}`);
+} catch (error) {
+  console.error(
+    `Unable to connect to the database: ${process.env.MYSQL_DATABASE}`,
+    error
+  );
+}
 
-module.exports = DB;
+module.exports = {
+  DB,
+};
